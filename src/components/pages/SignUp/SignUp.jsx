256b0cf82fc2059a5fbfed19e3565e9e7610d6c0
coderdivi <khosee6@gmail.com>
2023-06-06 18:46:48 +0100

    added instructor's api and css modules for the signup page

--- src/components/pages/SignUp/SignUp.jsx
+++ src/components/pages/SignUp/SignUp.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import Logo from "../../../assets/EducateLogo.png";
+import signup from "./SignUp.module.css";
+import logo from "../../../assets/EducateLogo.png";
+import RegisterImg from "../../../assets/register.png";
+
 import FormInput from "../../reusable/FormInput/FormInput";
-import { Link, useNavigate, /*useLocation*/} from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 
 export default function SignUp() {
   // *!State Management for the input field
@@ -57,105 +60,149 @@ export default function SignUp() {
       required: true,
     },
   ];
+  // useEffect(() => {
+  //   // Get the current URL
+  //   const currentURL = window.location.href;
 
+  //   // Extract the value of 'type' parameter
+  //   const urlSearchParams = new URLSearchParams(currentURL.split('?')[1]);
+  //   const userType = urlSearchParams.get('type');
+
+  //   console.log(userType); // Output: admin
+  // }, []);
   const handleSubmit = (e) => {
     e.preventDefault();
     console.dir(e.target[0]);
     passData();
-navigate("/login")
+    navigate("/login");
   };
   const onChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
   console.log(values);
 
+  
   // *! API call//
   const passData = () => {
-    console.log("passData before fetch");
-    fetch("https://edu-cate.onrender.com/api/v1/auth/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(values),
-    })
-      .then((response) => {
-        console.log("before response.ok", response);
-        if (response.ok) {
-          console.log("request successful");
-          return response.json();
-        } else {
-          throw new Error("API request failed");
-        }
+    const currentURL = window.location.href;
+    const searchParams = new URLSearchParams(currentURL.split('?')[1]);
+    const userType = searchParams.get("type");
+    if (userType === "admin") {
+      // navigate("/dashboard1");
+      console.log("passData after fetch");
+      fetch("https://edu-cate.onrender.com/api/v1/instructor/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
       })
-      .then((data) => {
-        console.log(data);
-        // navigate("./login");
+        .then((response) => {
+          console.log("before response.ok", response);
+          if (response.ok) {
+            console.log("request successful");
+            return response.json();
+          } else {
+            throw new Error("API request failed");
+          }
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else if (userType === "student") {
+      console.log("passData before fetch");
+      fetch("https://edu-cate.onrender.com/api/v1/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
       })
-      .catch((error) => {
-        console.error(error);
-      });
+        .then((response) => {
+          console.log("before response.ok", response);
+          if (response.ok) {
+            console.log("request successful");
+            return response.json();
+          } else {
+            throw new Error("API request failed");
+          }
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
   };
+
+
   // ! content box body
   return (
-    <section className="md:flex md:flex-row-reverse lg:flex lg:flex-row h-screen w-full justify-between">
-      <div className="w-full lg:w-1/2 py-3 px-2 md:px-14">
+    <section className={signup["sign-up-wrapper"]}>
+      <div className={signup.logoContent}>
         <img
-        className="h-10 w-fit"
-        src={Logo}
-        alt="Edu_cate"
+          src={logo}
+          alt="Edu_cate"
         />
-      <div className="mt-10 flex flex-col justify-start">
-          <form
-            className="formBox"
-            onSubmit={handleSubmit}
-          >
-            <h1 className="text-4xl font-semibold">Create an account</h1>
-            <p className="text-lg mt-2">Let's get you started</p>
-            {inputs.map((input) => (
-              <FormInput
-                key={input.id}
-                {...input}
-                value={values[input.name]}
-                onChange={onChange}
-              />
-            ))}
-            <div className="link-btn-wrapper">
-              {/* <Link to={`/signup-2`}> */}
-              <button
-                // onClick={ handleSignUp }
-                className="bg-orange-600 rounded-md p-2 text-white block w-full mt-10"
-                type="submit"
-              >
-                Sign Up
-              </button>
-            </div>
-            <span
-              className="SignIn"
-              style={{ marginTop: "2em" }}
-            >
-              Already have an account?
-              <Link
-                to={`/Login`}
-                style={{ color: "orangered", textDecoration: "none" }}
-              >
-                Sign In
-              </Link>
-            </span>
-          </form>
-        </div>
       </div>
-      <div className="hidden md:flex h-screen w-1/2 bg-no-repeat bg-cover bg-registerBg">
-        <div className="flex flex-col justify-end lg:px-10 py-36 text-xl text-white h-full">
-        <h1 
-          style={{ color: "white" }}>
-            <span className="font-bold text-3xl">Welcome to</span>
-            <span 
-            className="font-bold text-3xl"
-            style={{ color: "orangered" }}> Educate</span>
+      <div className={signup.contentBox}>
+        <form
+          className={signup.formBox}
+          onSubmit={handleSubmit}
+        >
+          <h1>Create an account</h1>
+          <p>Let's get you started</p>
+          {inputs.map((input) => (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={values[input.name]}
+              onChange={onChange}
+            />
+          ))}
+          <div className={signup["link-btn-wrapper"]}>
+            <button
+              className={signup.button1}
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </div>
+          <span
+            className={signup.SignIn}
+            style={{
+              marginTop: "10px",
+              fontSize: "10px",
+              display: "block",
+              textAlign: "center",
+            }}
+          >
+            Already have an account?
+            <Link
+              to={`/Login`}
+              style={{ color: "orangered", textDecoration: "none" }}
+            >
+              Sign In
+            </Link>
+          </span>
+        </form>
+      </div>
+      <div className={signup.imgBox}>
+        <div className={signup.BannerText}>
+          <h1 style={{ color: "white" }}>
+            Welcome to
+            <span style={{ color: "orangered" }}> Educate</span>
           </h1>
-          <p className="text-white/90">Sign up to find the best courses according to your preferences</p>
+          <p>Sign up to find the best courses according to your preferences</p>
         </div>
+        <img
+          src={RegisterImg}
+          alt=""
+        />
       </div>
     </section>
   );
