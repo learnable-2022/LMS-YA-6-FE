ac7a8581ff941be8ea1607a149b2615d2b9721e2
coderdivi <khosee6@gmail.com>
2023-06-15 18:24:36 +0100

    removed buttons and changed padding styles

--- src/components/reusable/landingpage-sections/EndSection.jsx
+++ src/components/reusable/landingpage-sections/EndSection.jsx
@@ -1,30 +1,26 @@
-const EndSection = () => {
-    return ( 
-        <div className="px-5 md:px-24 w-full lg:flex text-center lg:text-left bg-teachBg bg-no-repeat object-cover space-y-5 md:space-y-0 py-10">
-        <div className='flex flex-col justify-center lg:w-1/2 space-y-5'>
-            <h1 className='text-2xl text-white font-bold lg:w-1/2'>
-            We only offer you the best
-            </h1>
-        </div>
+import { useNavigate } from "react-router-dom";
 
-        <div className=' h-fit lg:w-1/2 space-y-2'>
-            <p className="text-white mb-5 md:mb-0">
-            Edu_cate, learning becomes flexible and accessible from anywhere with an 
-            internet connection. Students can access their course materials, 
-            assignments, and resources at their own convenience, allowing for 
-            self-paced learning.
-            </p>
-            <div className='flex space-x-10 justify-center lg:justify-normal'>
-                <div className='p-2 bg-orange-600 text-white rounded-md'>
-                Enroll Now
-                </div>
-                <div className='p-2 border-2 text-white border-white rounded-md'>
-                Sign In
-                </div>
-            </div>
+const EndSection = () => {
+  const navigate = useNavigate();
+    return (
+      <section className='px-5 md:px-14 w-full lg:flex text-center lg:text-left bg-teachBg bg-no-repeat bg-cover object-cover space-y-5 md:space-y-0 py-10 lg:py-20'>
+        <div className='flex flex-col justify-center lg:w-1/2 space-y-5'>
+          <h1 className='text-4xl text-white font-bold lg:w-1/2 capitalize'>We only offer you the best</h1>
         </div>
-    </div>
-     );
-}
- 
-export default EndSection;
+  
+        <div className='h-fit lg:w-1/2 space-y-2'>
+          <p className='text-white mb-5 md:mb-6 text-md lg:text-lg font-extralight'>
+            Edu_cate, learning becomes flexible and accessible from anywhere with an internet
+            connection. Students can access their course materials, assignments, and resources at
+            their own convenience, allowing for self-paced learning.
+          </p>
+          <div className='flex space-x-6 justify-center lg:justify-normal'>
+            <button className='p-2 bg-orange-600 text-white rounded-md'  onClick={() => navigate("/userAccess")}>Enroll Now</button>
+            {/* <button className='btn-outline text-white border-white'>Sign In</button> */}
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default EndSection;
