55c0840596626ac1e62193a3e6652c33a56ece95
coderdivi <khosee6@gmail.com>
2023-06-15 18:21:14 +0100

    Added routes

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,216 +1,177 @@
 import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../assets/EducateLogo.png";
-import { AiOutlineEyeInvisible, AiOutlineEye} from "react-icons/ai";
-import { useNavigate } from "react-router-dom";
+import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
+import * as api from "../../api";
 
-
-const Login = ()=> {
-  const [isPwdVisible, setIsPwdVisible] = useState(false)
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [disabled, setDisabled] = useState(false)
+const Login = () => {
+  const [isPwdVisible, setIsPwdVisible] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [disabled, setDisabled] = useState(false);
+  const [role, setRole] = useState("student");
   const navigate = useNavigate();
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    setDisabled(!disabled)
-    console.log('SUBMITED')
-    // const encryptedPassword = CryptoJS.SHA256(password).toString();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setDisabled(!disabled);
+
     const formData = {
-      'email':email, 
-      'password':password
-    }
-    console.log(formData)
-    const userType = localStorage.getItem('userType')
-    if(userType === 'instructor'){
-    fetchInstructorData(formData)
-   }else if(userType === 'student'){
-    fetchStudentData(formData)
-   }
-  }
-  useEffect(()=>{},[email, password])
+      email,
+      password,
+    };
 
-  // API call for an instructor
-      
-        const fetchInstructorData = async (formData)=>{
-          try {
-          const response = await fetch('https://edu-cate.onrender.com/api/v1/instructor/login',{
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData),
-          })
-        
-        if(response.ok){
-            console.log("request successful");
-            localStorage.setItem('userType', 'instructor')
-            console.log(response)
-            navigate("/instructor");
-            return response.json();
-          } else {
-                throw new Error("API request failed");
-                setDisabled(!disabled)
-          }
-      } catch (error) {
-        console.log(error)
+    try {
+      let data;
+      if (role === "student") {
+        data = await api.loginStudent(formData);
+      } else if (role === "instructor") {
+        data = await api.loginInstructor(formData);
       }
-    }
 
-    // Api call for a student
-    const fetchStudentData = async (formData)=>{
-      try {
-      const response = await fetch('https://edu-cate.onrender.com/api/v1/auth/login',{
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          // Authorization: ` Bearer `
-        },
-        body: JSON.stringify(formData),
-      })
-    
-    if(response.ok){
-        console.log("request successful");
-        localStorage.setItem('userType', 'student')
+      console.log(data);
+      if (data.data.role === "student") {
         navigate("/dashboard");
-        return response.json();
-      } else {
-            throw new Error("API request failed");
-            setDisabled(!disabled)
+      } else if (data.data.role === "instructor") {
+        navigate("/instructor");
       }
-  } catch (error) {
-    console.log(error)
-  }
-}
-    
-      // fetch("https://edu-cate.onrender.com/api/v1/instructor/login", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify(formData),
-      // })
-      //   .then((response) => {
-      //     console.log("before response.ok", response);
-      //     if (response.ok) {
-      //       console.log("request successful");
-      //       localStorage.setItem('userType', 'instructor')
-      //       navigate("/instructor");
-      //       return response.json();
-      //     } else {
-      //       throw new Error("API request failed");
-      //     }
-      //   })
-      //   .then((data) => {
-      //     console.log(data);
-      //   })
-      //   .catch((error) => {
-      //     console.error(error);
-      //   });
-  //   } else if (userType === "student") {
-  //     console.log("passData before fetch");
-  //     fetch("https://edu-cate.onrender.com/api/v1/auth/login", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(formData),
-  //     })
-  //       .then((response) => {
-  //         console.log("before response.ok", response);
-  //         if (response.ok) {
-  //           console.log("request successful");
-  //           localStorage.setItem('userType', 'student')
-  //           navigate("/dashboard");
-  //           return response.json();
-  //         } else {
-  //           throw new Error("API request failed");
-  //         }
-  //       })
-  //       .then((data) => {
-  //         console.log(data);
-  //       })
-  //       .catch((error) => {
-  //         console.error(error);
-  //       });
-    
-  // };
+      return data.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {}, [email, password]);
 
+  const handlePwdVisible = () => {
+    setIsPwdVisible(!isPwdVisible);
+  };
 
-
-
-  const handlePwdVisible = ()=>{
-    setIsPwdVisible(!isPwdVisible)
-  }
-      return (
-          <div className="md:flex md:flex-row-reverse lg:flex lg:flex-row h-screen w-full justify-between">
-              <div className="w-full lg:w-1/2 py-3 px-2 md:px-14">
-                <img src={Logo} alt="logo" className="h-10 w-fit" />
-                <div className="mt-10 flex flex-col justify-start">
-                  <form method="POST" onSubmit={handleSubmit}>
-                    <h1 className="text-4xl font-semibold mb-10">
-                      Login into your account
-                    </h1>
-                    <label htmlFor="email" className="relative block text-sm font-medium text-gray-700" >
-                      Email
-                    </label>
-                    <input 
-                      id="email" 
-                      type="email" 
-                      name="email" 
-                      required
-                      className="mb-5 w-full px-4 py-2 text-gray-700 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent" 
-                      placeholder="Enter your email address" 
-                      onChange={(e)=>setEmail(e.target.value)}
-                    />
-                      
-                      <label htmlFor="password" className="relative block text-sm font-medium text-gray-700">
-                      Password
-                      {
-                        isPwdVisible ? (
-                          < AiOutlineEyeInvisible className="absolute text-2xl right-5 top-7 cursor-pointer" onClick={handlePwdVisible} />
-                        ):
-                        (
-                          < AiOutlineEye className="absolute text-2xl right-5 top-7 cursor-pointer" onClick={handlePwdVisible} />
-                        )
-                      }
-                    </label>
-                    <input 
-                      id="password" 
-                      type={`${isPwdVisible ? "password" : "text"}`} 
-                      name="password" 
-                      required
-                      className="w-full px-4 py-2 text-gray-700 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent" 
-                      placeholder="Enter your password" 
-                      onChange={(e)=>setPassword(e.target.value)}
-                    />
-
-                    {
-                      disabled === true ? (
-                        <div className='mt-10 p-2 h-12 flex justify-center items-center bg-orange-600 text-white rounded-md cursor-pointer' style={{width: '100%'}}>
-                          <div className="inline-block animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
-                        </div>
-                      ):(
-                        <button type="submit" className='mt-10 p-2 h-12 flex justify-center items-center bg-orange-600 text-white rounded-md cursor-pointer' style={{width: '100%'}}>
-                        Sign In
-                        </button>
-                      )
-                    }
-                  </form>
-                </div>
-              </div>
-
-              <div className="hidden md:flex w-1/2 bg-no-repeat bg-loginBg flex-col justify-center lg:px-10 py-36 text-xl text-white h-full">
-              <h1 
-                style={{ color: "white" }}>
-                <span className="font-bold text-4xl">Welcome to</span>
-                <span 
-                className="font-bold text-4xl"
-                style={{ color: "orangered" }}> Educate</span>
-              </h1>
-          <p className="text-white/90">Sign up to find the best courses according to your preferences</p>
-              </div>
+  return (
+    <div className="md:flex md:flex-row-reverse lg:flex lg:flex-row h-screen w-full justify-between px-2 md:p-0">
+      <div className="grid w-full lg:w-1/2 py-3 px-2 md:px-14">
+        <div className="w-full justify-self-start">
+          <div className="flex justify-between items-center">
+            <Link to="/">
+              <img
+                src={Logo}
+                alt="logo"
+                className="h-10 w-fit"
+              />
+            </Link>
+            <div>
+              <select
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                className="px-4 py-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full"
+              >
+                <option value="student">Student</option>
+                <option value="instructor">Instructor</option>
+              </select>
+            </div>
           </div>
-      );
-}
+        </div>
+        <div className="mt-10 flex flex-col justify-start">
+          <form onSubmit={handleSubmit}>
+            <h1 className="text-4xl font-semibold mb-10">
+              Login into your account
+            </h1>
+            <label
+              htmlFor="email"
+              className="relative block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              required
+              className="mb-5 w-full px-4 py-2 text-gray-700 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent"
+              placeholder="Enter your email address"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <label
+              htmlFor="password"
+              className="relative block text-sm font-medium text-gray-700"
+            >
+              Password
+              {isPwdVisible ? (
+                <AiOutlineEyeInvisible
+                  className="absolute z-40 text-2xl right-5 top-7 cursor-pointer"
+                  onClick={handlePwdVisible}
+                />
+              ) : (
+                <AiOutlineEye
+                  className="absolute z-40 text-2xl right-5 top-7 cursor-pointer"
+                  onClick={handlePwdVisible}
+                />
+              )}
+            </label>
+            <input
+              id="password"
+              type={`${isPwdVisible ? "password" : "text"}`}
+              name="password"
+              required
+              className="w-full px-4 py-2 text-gray-700 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent"
+              placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {disabled === true ? (
+              <div
+                className="mt-10 p-2 h-12 flex justify-center items-center bg-orange-600 text-white rounded-md cursor-pointer"
+                style={{ width: "100%" }}
+              >
+                <div className="inline-block animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
+              </div>
+            ) : (
+              <button
+                type="submit"
+                className="mt-10 p-2 h-12 flex justify-center items-center bg-orange-600 text-white rounded-md cursor-pointer"
+                style={{ width: "100%" }}
+              >
+                Sign In
+              </button>
+            )}
+          </form>
+          <span
+            style={{
+              marginTop: "10px",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            Don't have an account?
+            <p
+              style={{ color: "orangered", textDecoration: "none" }}
+              className="cursor-pointer"
+              onClick={() => navigate("/userAccess")}
+            >
+              Sign Up
+            </p>
+          </span>
+        </div>
+      </div>
+
+      <div className="hidden md:flex w-1/2 bg-no-repeat bg-loginBg flex-col justify-center lg:px-10 py-36 text-xl text-white h-full">
+        <h1 style={{ color: "white" }}>
+          <span className="font-bold text-4xl">Welcome to</span>
+          <span
+            className="font-bold text-4xl"
+            style={{ color: "orangered" }}
+          >
+            {" "}
+            Educate
+          </span>
+        </h1>
+        <p className="text-white/90">
+          Sign up to find the best courses according to your preferences
+        </p>
+      </div>
+     
+    </div>
+  );
+};
 
 export default Login;
