1d54b5229378c391b39b81422fae108ef4b1f781
Morah Stephen <Morahstephen872@gmail.com>
2023-06-18 17:48:19 +0100

    fix: prettier

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../assets/EducateLogo.png";
 import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
-import { useNavigate } from "react-router-dom";
 import CryptoJS from "crypto-js";
 
 const Login = () => {
--- src/components/reusable/dashboard-sections/Hero.jsx
+++ src/components/reusable/dashboard-sections/Hero.jsx
@@ -1,66 +1,56 @@
-import Main from "./Main"
-import StatusCard from "./StatusCard";
+import Main from './Main';
+import StatusCard from './StatusCard';
 // import DynamicUsername from "./DynamicUsername/DynamicUsername"
-import React,{useEffect}from "react" ;
-import {studentProfile} from "./../../../api";
-
+import React, { useEffect } from 'react';
+import { studentProfile } from './../../../api';
 
 const Hero = () => {
-        useEffect(() => {
-        const fetchUsername = async () => {
-            const config = {
-                headers: {
-                  Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDhiNjBiYzZiOGQxMjIwNTRiMTkwMzkiLCJyb2xlIjoic3R1ZGVudCIsImlhdCI6MTY4NzAwODczMCwiZXhwIjoxNjg3NDQwNzMwfQ.XVNPGlm5Tb23WdoQ1GgW-o06Ln7RFI1RcPxoJJpn9TIs`,
-            
-                },
-              };
-              
-              try {
-                const { data } = await studentProfile(config);
-                console.log(data)
-                return data;
-              } catch (error) {
-                console.log(error);
-              }
-              
-          
-        };
-        
-        fetchUsername()
-    
-      }, [])
-    return ( 
-        <div className="lg:mx-10 w-full overflow-x-hidden">
-            <h1 className="py-5 flex flex-col p-2">
-                <span className="font-bold text-4xl">
-                    {/* Hello Kate <DynamicUsername/> */}
+  useEffect(() => {
+    const fetchUsername = async () => {
+      const config = {
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDhiNjBiYzZiOGQxMjIwNTRiMTkwMzkiLCJyb2xlIjoic3R1ZGVudCIsImlhdCI6MTY4NzAwODczMCwiZXhwIjoxNjg3NDQwNzMwfQ.XVNPGlm5Tb23WdoQ1GgW-o06Ln7RFI1RcPxoJJpn9TIs`,
+        },
+      };
 
-                    
-                    
-                </span>
-                <span>
-                    Welcome back, let’s dive right back in
-                </span>
-            </h1>
-            <div className="flex w-screen overflow-x-auto md:overflow-hidden items-center md:grid md:grid-cols-3 md:w-full h-fit p-2 md:p-0 md:space-x-8">
-                <div className="w-fit">
-                    <StatusCard courseNumber={8} courseStatus={'Completed Courses'} iconStatus={'check'} />
-                </div>
-                <div className="w-fit">
-                    <StatusCard courseNumber={5} courseStatus={'Course In Progress'} iconStatus={'todo'} />
-                </div>
-                <div className="w-fit">
-                    <StatusCard courseNumber={10} courseStatus={'Study Hours This Week'} iconStatus={'studyHours'} />
-                </div>
-            </div>
-            <div className="w-full space-y-2 px-2 overflow-x-scroll">
-                <h1 className="font-bold text-3xl">
-                    Courses
-                </h1>
-                <Main />
-            </div>
+      try {
+        const { data } = await studentProfile(config);
+        console.log(data);
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchUsername();
+  }, []);
+  return (
+    <div className='lg:mx-10 w-full overflow-x-hidden'>
+      <h1 className='py-5 flex flex-col p-2'>
+        <span className='font-bold text-4xl'>{/* Hello Kate <DynamicUsername/> */}</span>
+        <span>Welcome back, let’s dive right back in</span>
+      </h1>
+      <div className='flex w-screen overflow-x-auto md:overflow-hidden items-center md:grid md:grid-cols-3 md:w-full h-fit p-2 md:p-0 md:space-x-8'>
+        <div className='w-fit'>
+          <StatusCard courseNumber={8} courseStatus={'Completed Courses'} iconStatus={'check'} />
         </div>
-     );
-}
- 
+        <div className='w-fit'>
+          <StatusCard courseNumber={5} courseStatus={'Course In Progress'} iconStatus={'todo'} />
+        </div>
+        <div className='w-fit'>
+          <StatusCard
+            courseNumber={10}
+            courseStatus={'Study Hours This Week'}
+            iconStatus={'studyHours'}
+          />
+        </div>
+      </div>
+      <div className='w-full space-y-2 px-2 overflow-x-scroll'>
+        <h1 className='font-bold text-3xl'>Courses</h1>
+        <Main />
+      </div>
+    </div>
+  );
+};
+
 export default Hero;
