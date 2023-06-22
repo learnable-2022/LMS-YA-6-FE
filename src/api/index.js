709a9d4d2c6ed41704e82941c9dd2daf4c33a240
Morah Stephen <Morahstephen872@gmail.com>
2023-06-22 16:22:43 +0100

    feat: course create

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:1337/api/v1'
-  // baseURL: 'https://edu-cate.onrender.com/api/v1'
+  // baseURL: 'http://localhost:1337/api/v1'
+  baseURL: 'https://edu-cate.onrender.com/api/v1'
 });
 
 export const getCourses = () => API.get('/courses');
@@ -12,7 +12,7 @@ export const progressodule = (courseId, moduleId, config) => API.patch(`/courses
 
 export const loginStudent = (formData) => API.post('/auth/login', formData);
 export const registerStudent = (formData) => API.post('/auth/register', formData);
-export const studentProfile = () => API.get('/student/profile');
+export const studentProfile = (config) => API.get('/student/profile', config);
 
 export const loginInstructor = (formData) => API.post('/instructor/login', formData);
 export const registerInstructor = (formData) => API.post('/instructor/register', formData);
