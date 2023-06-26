46d0b1e1afeb566d43928b84df40dfb7eb42d342
Arnold <86104635+iArney@users.noreply.github.com>
2023-06-26 12:05:41 +0300

    Update AllCourses

--- src/components/pages/dashboard-pages/AllCourses.jsx
+++ src/components/pages/dashboard-pages/AllCourses.jsx
@@ -1,6 +1,7 @@
 import Footer from '../../reusable/Footer';
 import CourseContents from '../../reusable/all-courses/CourseContents';
 import Courses from '../../reusable/all-courses/Courses';
+import UpperSection from '../../reusable/all-courses/UpperSection';
 import Main from '../../reusable/dashboard-sections/Main';
 import SideNavbar from '../../reusable/dashboard-sections/SideNavbar';
 import Topbar from '../../reusable/dashboard-sections/Topbar';
@@ -12,7 +13,12 @@ const AllCourses = () => {
       <div className='flex h-fit min-h-screen '>
         <SideNavbar studentRoute={'/all-courses'} />
         <div className='md:w-4/5 px-2 md:px-8 mb-5 mt-10'>
+          <div className='mb-5'>
+          <UpperSection />
+          </div>
+        <div>
         <Main />
+        </div>
         </div>
       </div>
       <Footer />
