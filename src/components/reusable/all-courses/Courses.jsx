4ff39cfc584eeb0c77477979482016b199d47a76
Arnold <86104635+iArney@users.noreply.github.com>
2023-06-26 12:07:34 +0300

    Update Courses

--- src/components/reusable/all-courses/Courses.jsx
+++ src/components/reusable/all-courses/Courses.jsx
@@ -5,7 +5,7 @@ import UpperSection from './UpperSection';
 const Courses = ({ cancelEvents }) => {
   return (
     <div className='lg:px-10 px-2 py-6 overflow-hidden'>
-      <UpperSection />
+{/*       <UpperSection /> */}
       <div className='mt-10'>
         <Filter />
         <CourseContents />
